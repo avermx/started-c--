f53e33758e933c2b891869dd6361baad1e27ddf6
avermx <aniket125verma@gmail.com>
2025-02-16 10:15:50 +0530

    init

---
+++ day-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ using namespace std;
+
+int main(){
+
+
+ int num = 20;
+ if(num%8==0)
+ {
+    if(num%16==0){
+        cout<<"prefect";
+    };
+ }
+
+ else cout<<"not perfect";
+ 
+ 
+}
+
