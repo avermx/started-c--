fd79947a719cd2f9cf4ea6958073cc11b2e14f1b
avermx <aniket125verma@gmail.com>
2025-02-23 20:05:53 +0530

    init

--- day6/day6.cpp
+++ day6/day6.cpp
@@ -5,18 +5,27 @@ using namespace std;
 
 int main()
 {
-    for (int i = 1; i <= 5; i++)
-    {
-        char c = 'a' + i-1;
-        for (int j = 1; j <=5; j++)
+    for (int i = 5; i >= 1; i--)
+    {                   
+        for (int s = i+1; s<=5; s++)
+        {
+            cout<<" ";
+        
+
+        }
+        for (int j = 1; j <=i; j++)
         {
            
-            cout<<c<<" ";
-
+            
+            cout<<"*"<<" ";
 
         };
+ 
+    
     
         cout<<endl;
     }
     return 0;
 }
+
+
