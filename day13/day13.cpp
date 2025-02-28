be549b90d780bc12404cd9d120bf7953385495b8
avermx <aniket125verma@gmail.com>
2025-02-28 22:08:10 +0530

    init

---
+++ day13/day13.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main ()
+{
+    int num;
+    cin>>num;
+    int an = 1; 
+    for (int i = 1; i <=num; i++)
+    
+    {
+        for (int j = 1; j<=i; j++)
+        {
+            
+            cout<<an;
+            an++;
+            
+        }
+
+        cout<<endl;
+    }
+          
+}      
+
+
+
