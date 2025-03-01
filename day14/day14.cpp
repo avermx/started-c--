7d6d84692f3e680de36ac53c4f09fe02e1b94026
avermx <aniket125verma@gmail.com>
2025-03-02 00:15:02 +0530

    init

--- day14/day14.cpp
+++ day14/day14.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 
 int main(){
-    int num = 1;
+    int num;
     for (int i = 1; i <=5; i++)
     {
+        if(i%2==0) num = 1;
+        else num = 0;
         for (int j = 1;  j<i; j++)
         {
-             cout<<num<<" ";
-             num = num + 2;
+             cout<<num;
+            if(num==0) num=1;
+            else num = 0;
         }
         cout<<endl;
     }
