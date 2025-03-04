17fb8adf44b25e3d42aaff5b7aec0e3ed7f2ae9e
avermx <aniket125verma@gmail.com>
2025-03-05 02:07:36 +0530

    init

--- day14/day14.cpp
+++ day14/day14.cpp
@@ -4,31 +4,33 @@ using namespace std;
 
 
 int main(){
-
-    // for (int i = 1; i <=5; i++)
-    // {
-    //     if(i%2!=0) num = 1;
-    //     else num = 0;
-    //     for (int j = 1;  j<i; j++)
-    //     {
-    //          cout<<num;
-    //         if(num==0) num=1;
-    //         else num = 0;
-    //     }
-
-for(int row = 1; row<=5; row++)
-{
-    for (int col = 1; col<=5; col++)
+    int num;
+    cin>>num;
+    for (int i = 1; i <=5; i++)
     {
-       if(row==1 || col==1 || row==5 || col==5){
-        cout<<"*";
-       }
-       else{
-        cout<<" ";
-       }
-    }
+        if(i%2!=0) num = 1;
+        else num = 0;
+        for (int j = 1;  j<i; j++)
+        {
+             cout<<num;
+            if(num==0) num=1;
+            else num = 0;
+        }
 
-        cout<<endl;
+// for(int row = 1; row<=5; row++)
+// {
+//     for (int col = 1; col<=5; col++)
+//     {
+//        if(row==1 || col==1 || row==5 || col==5){
+//         cout<<"*";
+//        }
+//        else{
+//         cout<<" ";
+//        }
+//     }
+
+//         cout<<endl;
     
+// }
 }
 }
