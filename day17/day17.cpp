92bb151b89329b3781d0cd9878f7f3836b772a9e
avermx <aniket125verma@gmail.com>
2025-03-05 19:44:15 +0530

    init

---
+++ day17/day17.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+     int a = 1;
+    for(int i = 1; i<=5; i++){
+        for (int j = 1; j<=a; j++)
+        {
+            cout<<"#";
+            
+        }
+        a = a + 2;
+        cout<<endl;
+        
+    }
+}
