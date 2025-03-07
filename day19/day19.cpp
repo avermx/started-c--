bb906b6c66f0e2b7db8a9b0d1f3e0440f83c4005
avermx <aniket125verma@gmail.com>
2025-03-08 00:59:14 +0530

    int

---
+++ day19/day19.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int n; 
+    cin>>n;
+    for(int i = 1; i<=n; i++)
+    {
+        char w = 65;
+        for(int j = 1; j<=n-i; j++)
+        {
+            cout<<"#";
+        }
+        for(int k = 1; k<=i; k++)
+        {
+            cout<<w;
+            w++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
