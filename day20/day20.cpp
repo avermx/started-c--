2e793764c7c53b4df9dc3267230807a61c9a81ce
avermx <aniket125verma@gmail.com>
2025-03-09 22:52:24 +0530

    int

--- day20/day20.cpp
+++ day20/day20.cpp
@@ -12,19 +12,14 @@ int main (){
 
         for (int j = 1; j <=n; j++)
         {
-           if(i==3 || j==3){
-            cout<<"*";
-           }
-           else{
-            cout<<" ";
-           }
+            cout<<a;
+            a++;
         }
         
        
         cout<<endl;
     }
     
-
 
 
 
