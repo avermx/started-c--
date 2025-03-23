be8b9d68d6ece56bcca5f559947cdb066156e6cd
avermx <aniket125verma@gmail.com>
2025-03-24 00:54:01 +0530

    init

--- day28/day28.cpp
+++ day28/day28.cpp
@@ -1,37 +1,50 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
 
 
-int main(){
-    int n;
-    cin>>n;
-    int nsp = 1;
-    int nst = n;
+// int main(){
+//     // int n;
+//     // cin>>n;
+//     // int nsp = 1;
+//     // int nst = n;
     
-    for (int q = 1; q<=2*n+1; q++)
-    {
-       cout<<q;
-    }
-    cout<<endl;
-    for(int i =1; i<=n; i++){
-        int a = 1;
-        for(int j = 1; j<=nst; j++){
-            cout<<a;
-            a++;
-        }
-        for(int k = 1;k<=nsp; k++){
-            cout<<" ";
-            a++;
-        }
-        for(int j = 1; j<=nst; j++){
-            cout<<a;
-            a++;
-        }
-        nsp+=2;
-        nst--;
-        cout<<endl;
-    }
+//     // for (int q = 1; q<=2*n+1; q++)
+//     // {
+//     //    cout<<q;
+//     // }
+//     // cout<<endl;
+//     // for(int i =1; i<=n; i++){
+//     //     int a = 1;
+//     //     for(int j = 1; j<=nst; j++){
+//     //         cout<<a;
+//     //         a++;
+//     //     }
+//     //     for(int k = 1;k<=nsp; k++){
+//     //         cout<<" ";
+//     //         a++;
+//     //     }
+//     //     for(int j = 1; j<=nst; j++){
+//     //         cout<<a;
+//     //         a++;
+//     //     }
+//     //     nsp+=2;
+//     //     nst--;
+//     //     cout<<endl;
+//     // }
     
-    return 0;
-}
+//     // return 0;
+
+//     char a = '*';
+
+//     for(int col = 1; col<=5; col++){
+//         for (int row = 1; row <=col; row++)
+//         {
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+
+
+    
+// }
---
+++ day29/day29.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int abc(int a,int b){
+    return a + b;
+}
+int main(){
+
+    
+    cout<<abc(12,25);
+    return 0;
+}
