94cebe81bc688afcc4bdbb6ff7ec312dc4b3fdf2
avermx <aniket125verma@gmail.com>
2025-04-11 01:31:23 +0530

    init

---
+++ day38/day38.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int arr[5] = {1, 2, 3, 4, 5};
+    // int Total = 0;
+    // for (int i = 0; i <= 4; i++)
+    // {
+    //     for (int j = i + 1; j <= 4; j++)
+    //     {
+
+    //         for (int k = j + 1; k <= 4; k++)
+    //         {
+    //             if(arr[i]+arr[j]+arr[k] == 12){
+    //                 Total++;
+    //                 cout<<arr[i],arr[j],arr[k];
+    //                 cout<<endl;
+    //             }
+    //         }
+    //     }
+    // }
+    // cout<<Total;
+
+    // int arr[5] = {1,2,3,4,5};
+    // int max = arr[0];
+    // int min = arr[0];
+
+    // for (int i = 0; i <= 4; i++)
+    // {
+    //     if (arr[i]>max)
+    //     {
+    //         min = max;
+    //         max = arr[i];
+       
+    //     }
+        
+    // }
+    
+    // cout<<min;
+
+
+
+    // int arr[6] = {1,2,3,3,3,1};
+
+    // int max = arr[0];
+    // int smax = arr[0];
+
+    // for(int i = 0; i<=5; i++){
+    //     if(max<arr[i]){
+    //         smax = max;
+    //         max = arr[i];
+    //     }
+    //     if (smax<arr[i] && max!=arr[i]){
+    //         smax = arr[i];
+    //     }
+    // }
+    // cout<<smax;
+    
+}
