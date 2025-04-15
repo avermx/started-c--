d9e5cde75f33b644b728220b61fe1b626adea93a
avermx <aniket125verma@gmail.com>
2025-04-16 01:22:56 +0530

    init

--- day43/day43.cpp
+++ day43/day43.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 
 int main(){
-    int arr[2][2] = {{},{}};
+    int arr[3][2] = {{1,2},{3,4},{7,8}};
+    for (int i = 0; i<3; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout<<arr[i][j];
+        }
+        cout<<endl;
+    }
+    
 }
