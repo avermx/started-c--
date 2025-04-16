8cfbc20a47de11223843fcfffa22665aad33b955
avermx <aniket125verma@gmail.com>
2025-04-17 02:23:51 +0530

    init

--- day45/day45.cpp
+++ day45/day45.cpp
@@ -5,24 +5,143 @@ using namespace std;
 int main()
 {
     // int arr[5][5] = {{10,10,10,10,10,},{10,10,10,10,10},{10,10,10,10,10},{10,10,10,10,10},{10,10,10,10,10}};
-    int arr[5][5];
+    // int arr[5][5];
 
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            arr[i][j] = 10;
-        }
-        cout << endl;
-    }
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            cout<<arr[i][j];
-        }
-        cout <<endl;
-    }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     for (int j = 0; j < 5; j++)
+    //     {
+    //         arr[i][j] = 10;
+    //     }
+    //     cout << endl;
+    // }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     for (int j = 0; j < 5; j++)
+    //     {
+    //         cout<<arr[i][j];
+    //     }
+    //     cout <<endl;
+    // }
+
+    // int arr[2][2];
+    // int brr[2][2];
+    // int jrr[2][2];
+
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     for (int j = 0; j < 2; j++){
+
+    //         arr[i][j] = arr[i][j]+brr[i][j];
+    //         cout<<arr[i][j];
+
+    //     }
+    //     cout<<endl;
+
+    // }
+
+    // int arr[2][2];
+    // int brr[2][2];
+    // int jrr[2][2];
+
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     for (int j = 0; j < 2; j++)
+    //     {
+    //         cout<<"Enter The Element",i,j;
+    //         cin>>arr[i][j];
+    //     }
+    //     for (int j = 0; j < 2; j++)
+    //     {
+    //         cout<<"Enter The Element",i,j;
+    //         cin>>brr[i][j];
+    //     }
+
+    // }
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     for (int j = 0; j < 2; j++)
+    //     {
+    //         cout<<arr[i][j];
+
+    //     }
+    //     cout<<endl;
+    //     cout<<endl;
+    //     for (int j = 0; j < 2; j++)
+    //     {
+    //         cout<<brr[i][j];
+    //     }
+    //     cout<<endl;
+
+    // }
+    // int r;
+    // int c;
+    // cout << "Enter Row";
+    // cin >> r;
+    // cout << "Enter Column";
+    // cin >> c;
+    // int arr[r][c];
+
+    // for (int i = 0; i < r; i++)
+    // {
+    //     for (int j = 0; j < c; j++)
+    //     {
+    //         cin >> arr[i][j];
+    //     }
+    // }
+    // int sum = 0;
+    // for (int i = 0; i < r; i++)
+    // {
+    //     for (int j = 0; j < c; j++)
+    //     {
+    //         sum += arr[i][j];
+    //         cout << arr[i][j];
+    //     }
+    //     cout << endl;
+    //     cout << endl;
+    // }
+    // cout<<sum;
+
+    // For Max Element;
+    // int arr[2][2] = {{1, 2}, {5, 4}};
+    // int max = arr[0][0];
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     for (int j = 0; j < 2; j++)
+    //     {
+    //         if(max<arr[i][j]){
+    //             max = arr[i][j];
+
+    //         }
+    //     }
+    // }
+    // cout<<max;
+
+    // for Min Element
     
-   
+    // int arr[2][2] = {{2, 1}, {5, 4}};
+    // int min = arr[0][0];
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     for (int j = 0; j < 2; j++)
+    //     {
+    //         if (min > arr[i][j])
+    //         {
+    //             min = arr[i][j];
+    //         }
+    //     }
+    // }
+    // cout << min;
+
+    // int arr[3] = {1,2,3};
+    // int max = arr[0];
+    // for (int i = 0; i <=2; i++)
+    // {
+    //     if (max<arr[i])
+    //     {
+    //         max = arr[i];
+    //     }
+
+    // }
+    // cout<<max;
 }
