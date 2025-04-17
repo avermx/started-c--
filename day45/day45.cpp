06142e89513182ca63f6c7bd292609d1ec0204a2
avermx <aniket125verma@gmail.com>
2025-04-18 00:30:45 +0530

    init

--- day45/day45.cpp
+++ day45/day45.cpp
@@ -252,4 +252,23 @@ int main()
     // cout << max;
     // cout << endl;
     // cout << "Row Number " << row;
+
+    int arr[3][4] = {{1, 0, 0, 1}, {1, 0, 1, 0}, {1, 1, 0, 1}};
+    int maxOne = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < 4; j++)
+    
+        {
+            if (arr[i][j] == 1)
+            {
+                sum++;
+                if(maxOne<sum){
+                    maxOne = sum;
+                }
+            }
+        }
+    }
+    cout<<maxOne;
 }
