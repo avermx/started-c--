2eece01f9fad300c40acb1b81f45fc4a103b61ba
avermx <aniket125verma@gmail.com>
2025-04-19 14:36:13 +0530

    muti of matrix

---
+++ day46/day46.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    // int brr[3][2];
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 2; j++)
+    //     {
+    //        brr[i][j] = arr[j][i];
+    //     }
+
+    // }
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 2; j++)
+    //     {
+    //       cout<<brr[i][j];
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int n;
+    // cout<<"Enter The Number Of Row/Column";
+    // cin>>n;
+    // int arr[n][n];
+
+    // for(int i = 0; i<n; i++){
+    //     for (int j = 0; j <n; j++)
+    //     {
+    //         cin>>arr[i][j];
+
+    //     }
+    // }
+
+    // for(int i = 0; i<n; i++){
+    //     for (int j = i; j <n; j++)
+    //     {
+    //         int temp = arr[i][j];
+    //         arr[i][j] = arr[j][i];
+    //         arr[j][i] = temp;
+
+    //     }
+    // }
+
+    // for(int i = 0; i<n; i++){
+    //     for (int j = 0; j <n; j++)
+    //     {
+    //         cout<<arr[i][j] ;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = i; j < 3; j++)
+    //     {
+    //         int temp = arr[i][j];
+    //         arr[i][j] = arr[j][i];
+    //         arr[j][i] = temp;
+    //     }
+    // }
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //     {
+    //         cout << arr[i][j];
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     int j = 0;
+    //     int k = 2;
+
+    //     while (j < k)
+    //     {
+    //         int temp = arr[i][j];
+    //         arr[i][j] = arr[i][k];
+    //         arr[i][k] = temp;
+    //         j++;
+    //         k--;
+
+    //     }
+    // }
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //     {
+    //         cout << arr[i][j];
+    //     }
+    //     cout << endl;
+    // }
+
+
+    
+    // multi of matrix
+
+
+    // int a[2][2] = {{1, 0}, {0, 1}};
+    // int b[2][2] = {{0, 1}, {1, 0}};
+    // int n[2][2];
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     for (int j = 0; j < 2; j++)
+    //     {
+    //         n[i][j] = 0;
+    //         for (int k = 0; k < 2; k++)
+    //         {
+    //             n[i][j] += a[i][k] * b[k][j];
+    //         }
+    //     }
+    // }
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     for (int j = 0; j < 2; j++)
+    //     {
+    //         cout << n[i][j];
+    //     }
+    //     cout << endl;
+    // }
+}
