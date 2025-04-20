6156edb2b389ecaa14ddd9d7b5f075670bc2aeef
avermx <aniket125verma@gmail.com>
2025-04-20 20:58:25 +0530

    init

--- day47/day47.cpp
+++ day47/day47.cpp
@@ -5,37 +5,55 @@ int main()
 {
     int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-    for (int i = 0; i < 3; i++)
-    {
-        
-       if(i==0){
-        int temp = arr[i][i];
-        arr[i][i] = arr[2][i];
-        arr[2][i] = temp;
-       }
-       else if(i==2) {
-        int temp = arr[i][i];
-        arr[i][i] = arr[0][i];
-        arr[0][i] = temp;
-       }
-    
-    }
-    cout<<endl;
- 
+    // for (int i = 0; i < 3; i++)
+    // {
+
+    //    if(i==0){
+    //     int temp = arr[i][i];
+    //     arr[i][i] = arr[2][i];
+    //     arr[2][i] = temp;
+    //    }
+    //    else if(i==2) {
+    //     int temp = arr[i][i];
+    //     arr[i][i] = arr[0][i];
+    //     arr[0][i] = temp;
+    //    }
+
+    // }
+    // cout<<endl;
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //     {
+    //         cout<<arr[i][j];
+    //     }
+    //     cout<<endl;
+
+    // }
+
+    int brr[3][3];
+
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            cout<<arr[i][j];
+            brr[i][j] = arr[j][i];
         }
-        cout<<endl;
-    zz
+        cout << endl;
     }
 
-} 
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << brr[i][j];
+        }
+        cout << endl;
+        
+    }
+}
 
-
-
-// 7 2 9   1 4 7  1 2 3  
-// 4 5 6   2 5 8  4 5 6  
+// 7 2 9   1 4 7  1 2 3
+// 4 5 6   2 5 8  4 5 6
 // 1 8 3   3 6 9  7 8 9
